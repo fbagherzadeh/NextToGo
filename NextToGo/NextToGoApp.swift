da9bbe1cb092392a8565e0560279c4ddfb01c8b2
Farhad Bagherzadeh <farhad.bagherzadeh@rea-group.com>
2025-05-24 11:11:46 +1000

    Initial Commit

---
+++ NextToGo/NextToGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextToGoApp.swift
+//  NextToGo
+//
+//  Created by Farhad Bagherzadeh on 24/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct NextToGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
